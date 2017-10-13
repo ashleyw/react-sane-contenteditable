b36c2ad530a90056e4ed77545907ee2a5fe72c6c
Ashley Williams <hi@ashleyw.co.uk>
2017-10-13 12:25:00 +0100

    Store the raw value internally and always output the sanitised value via the onChange callback

--- src/react-sane-contenteditable.js
+++ src/react-sane-contenteditable.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { omit } from 'lodash';
+import { omit, isEqual, pick } from 'lodash';
 
 const propTypes = {
   content: PropTypes.string,
   editable: PropTypes.bool,
   maxLength: PropTypes.number,
-  mode: PropTypes.oneOf(['plaintext', 'html']),
   multiLine: PropTypes.bool,
   onChange: PropTypes.func,
+  sanitise: PropTypes.bool,
   tagName: PropTypes.string,
 }
 
@@ -16,80 +16,100 @@ export default class ContentEditable extends Component {
   static propTypes = propTypes;
 
   static defaultProps = {
+    content: '',
     editable: true,
     maxLength: Infinity,
-    mode: 'plaintext',
     multiLine: false,
+    sanitise: true,
     tagName: 'div',
   }
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: props.content,
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.content !== this.cleanOutput(this.state.value)) {
+      this.setState({ value: nextProps.content }, this.forceUpdate);
+    }
+  }
+
   shouldComponentUpdate(nextProps) {
-    const method = this.getInnerMethod();
-    return nextProps.content !== this._element[method];
+    const propKeys = omit(Object.keys(propTypes), ['content']);
+
+    return !isEqual(
+      pick(nextProps, propKeys),
+      pick(this.props, propKeys)
+    );
   }
 
+  cleanOutput(val) {
+    const { maxLength, multiLine, sanitise } = this.props;
 
-  getInnerMethod() {
-    return this.props.mode === 'plaintext' ? 'innerText' : 'innerHTML';
-  }
-
-  _onChange = (ev) => {
-    const method = this.getInnerMethod();
-    const value = this._element[method];
-
-    this.props.onChange(ev, value);
-  }
-
-  sanatisedValue() {
-    const { maxLength, multiLine } = this.props;
-
-    const method = this.getInnerMethod();
+    if (!sanitise) {
+      return val;
+    }
 
     // replace encoded spaces
-    let value = this._element[method].replace(/&nbsp;/g, ' ');
+    let value = val.replace(/&nbsp;/g, ' ');
 
     if (multiLine) {
-      // replace any any number of whitespace characters (other than new lines!) with a single space
+      // replace any 2+ character whitespace (other than new lines) with a single space
       value = value.replace(/[\t\v\f\r \u00a0\u2000-\u200b\u2028-\u2029\u3000]+/g, ' ');
     } else {
-      // replace any number of whitespace characters with a single space
       value = value.replace(/\s+/g, ' ');
     }
 
     return value.split('\n')
       .map(line => line.trim())
       .join('\n')
-      .replace(/\n{3,}/g, '\n\n')
+      .replace(/\n{3,}/g, '\n\n') // replace 3+ linebreaks with two
       .trim()
       .substr(0, maxLength);
   }
 
-  _onPaste = (ev) => {
-    const { maxLength, mode } = this.props;
+  _onChange = (ev) => {
+    const value = this.refs.element.innerText;
 
-    if (mode === 'plaintext') {
-      ev.preventDefault();
-      const text = ev.clipboardData.getData('text').substr(0, maxLength);
-      document.execCommand('insertText', false, text);
+    if (this.state.value !== value) {
+      this.setState({ value }, () => {
+        this.props.onChange(ev, this.cleanOutput(value));
+      });
     }
   }
 
+  _onPaste = (ev) => {
+    const { maxLength } = this.props;
+
+    ev.preventDefault();
+    const text = ev.clipboardData.getData('text').substr(0, maxLength);
+    document.execCommand('insertText', false, text);
+  }
+
   _onBlur = (ev) => {
-    this.props.onChange(ev, this.sanatisedValue());
+    const value = this.cleanOutput(this.refs.element.innerText);
+
+    this.setState({ value }, () => {
+      this.forceUpdate();
+      this.props.onChange(ev, value);
+    });
   }
 
   _onKeyDown = (ev) => {
     const { maxLength, multiLine } = this.props;
-    const method = this.getInnerMethod();
-    const value = this._element[method];
+    const value = this.refs.element.innerText;
 
-    // keyCode 13 === return key
+    // return key
     if (!multiLine && ev.keyCode === 13) {
       ev.preventDefault();
       ev.currentTarget.blur();
     }
 
-    // Ensure we don't exceed `maxLength` (keyCode 8 === backspace)
+    // Ensure we don't exceed `maxLength` (keycode 8 === backspace)
     if (maxLength && !ev.metaKey && ev.which !== 8 && value.replace(/\s\s/g, ' ').length >= maxLength) {
       ev.preventDefault();
     }
@@ -101,10 +121,10 @@ export default class ContentEditable extends Component {
     return (
       <Element
         {...omit(props, Object.keys(propTypes))}
-        ref={c => { this._element = c; }}
+        ref="element"
         style={{ whiteSpace: 'pre-wrap', ...props.style }}
         contentEditable={editable}
-        dangerouslySetInnerHTML={{ __html: content.replace(/[\t\v\f\r \u00a0\u2000-\u200b\u2028-\u2029\u3000]{2}/g, ' &nbsp;') }}
+        dangerouslySetInnerHTML={{ __html: this.state.value }}
         onBlur={this._onBlur}
         onInput={this._onChange}
         onKeyDown={this._onKeyDown}
